a510e6e8fcf2dcbe73b72cc08d38ab5dd77070ba
Shiva Saxena <shivasaxena@outlook.com>
2022-02-24 03:24:30 +0530

    Initial commit

---
+++ src/main/java/com/metalop/springbootolingo4sampleproject/SpringBootOlingo4SampleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.metalop.springbootolingo4sampleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOlingo4SampleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOlingo4SampleProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metalop/springbootolingo4sampleproject/SpringBootOlingo4SampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metalop.springbootolingo4sampleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOlingo4SampleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
